b507f5587b3b5cc4d9ebdfe5ec51e58a35e2a40e
Putu Rizki Manik Widiadnyana <putu.rizki@ui.ac.id>
2026-09-16 22:09:54 +0700

    Feat: adding, deleting and searching skills

--- main/urls.py
+++ main/urls.py
@@ -9,23 +9,13 @@ from main.views import (
     show_skills,
 )
 
-urlpatterns = [
-    path("", show_main, name="show_main"),
-    path("experience/", show_experience, name="show_experience"),
-    path("skills/", show_skills, name="show_skills"),
-    path("skills/add/", create_skill, name="create_skill"),
-    path(
-        "skills/<uuid:skill_id>/delete/",
-        delete_skill,
-        name="delete_skill",
-    ),
-    path("api/skills/", get_skills_json, name="get_skills_json"),
-]
-
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("skills/", show_skills, name="show_skills"),
+    path("skills/add/", create_skill, name="create_skill"),
+    path("skills/<uuid:skill_id>/delete/", delete_skill, name="delete_skill"),
+    path("api/skills/", get_skills_json, name="get_skills_json"),
 ]
